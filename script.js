089e39b5bf2f7d99f0d9c75ad0a5713eb96245dd
Johanray0704 <johanlawrenceg@gmail.com>
2025-02-21 17:27:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-function login(){
-    var username=document.getElementById("username").value
-    var password=document.getElementById("password").value
-    var errormessage=document.getElementById("error-msg").value
-    if(username=="johan"&&password=="12345"){
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var errormessage = document.getElementById("error-msg"); 
+
+    if (username == "johan" && password == "12345") {
         alert("Login successfully");
-    }
-    else{
-        errormessage.textContent="Invalid username or password";
+        errormessage.textContent = ""; 
+    } else {
+        errormessage.textContent = "Invalid username or password";
     }
 }
